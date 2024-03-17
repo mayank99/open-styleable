9d4442295e911d0b5637cf8bb8258f9563e8a75a
Mayank <9084735+mayank99@users.noreply.github.com>
2024-03-17 15:46:17 -0400

    setup webc correctly

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,16 @@
+import pluginWebc from "@11ty/eleventy-plugin-webc";
+
 export default function (eleventyConfig) {
-	eleventyConfig.ignores.add('README.md');
+	eleventyConfig.ignores.add("README.md");
+	eleventyConfig.addPlugin(pluginWebc, {
+		components: ["_components/**/*.webc"],
+	});
 
 	return {
+		htmlTemplateEngine: "webc",
 		dir: {
 			input: "pages",
-			output: "dist"
-		}
-	}
+			output: "dist",
+		},
+	};
 }
