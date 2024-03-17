87b2d4c944e8d8259f11ffb2d831c1bc4dd55fad
Mayank <9084735+mayank99@users.noreply.github.com>
2024-03-17 16:58:51 -0400

    typedefs for config

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,8 @@
 import pluginWebc from "@11ty/eleventy-plugin-webc";
 
+/** @param {import("@11ty/eleventy/src/UserConfig.js").default} eleventyConfig */
 export default function (eleventyConfig) {
-	eleventyConfig.ignores.add("README.md");
+	eleventyConfig.ignores?.add("README.md");
 	eleventyConfig.addPlugin(pluginWebc, {
 		components: ["_components/**/*.webc"],
 	});
